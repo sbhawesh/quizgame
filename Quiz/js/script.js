d399ff2da1d52a52dbd188d0727799364aca3318
sbhawesh <31862771+sbhawesh@users.noreply.github.com>
2017-09-17 02:05:56 +0530

    Update script.js

--- Quiz/js/script.js
+++ Quiz/js/script.js
@@ -1,25 +1,28 @@
-function submitAnswer(){
+function submitAnswers()
+{
 	 var total = 5;
 	 var score = 0;
 
-	 var q = [0];
 	 
-	 q.push(document.forms["quizForm"]["q1"].value);
-	 q.push(document.forms["quizForm"]["q2"].value);
-	 q.push(document.forms["quizForm"]["q3"].value);
-	 q.push(document.forms["quizForm"]["q4"].value);
-	 q.push(document.forms["quizForm"]["q5"].value);
+	 var q1 = document.forms["quizForm"]["q1"].value;
+	 var q2 = document.forms["quizForm"]["q2"].value;
+	 var q3 = document.forms["quizForm"]["q3"].value;
+	 var q4 = document.forms["quizForm"]["q4"].value;
+	 var q5 = document.forms["quizForm"]["q5"].value;
 	 
-	for(i = 1;i <= total;i++){
+	for(i = 1;i <= total;i++)
+	{
 		
-	  if(q[i]==null){
-		  alert("missed question no.",+i);
-	  }
+	  if(eval('q'+i) ==null || eval('q'+i) =='')
+	  {
+		 
+			alert('missed question number ' + i);
+	  return false;
 	 }
-	 
 	}
+	
 	//Answers
-	 var answers = ["a","c","c","a","b"];
+	 var answers = ["a","c","c","b","b"];
 	 for(i = 1; i <= total;i++){
 	    if(eval('q'+i) == answers[i-1]){
 		  score++;
@@ -27,10 +30,10 @@ function submitAnswer(){
 	 }
 	 
 	 var results = document.getElementById('results');
-	 results.innerHTML = '<h3>You scored <span>'+score+'</span>out of<span>'+total+'</span></h3>';
+	 results.innerHTML = '<h3>You scored <span> '+score+'</span> out of <span>'+total+'</span></h3>';
 	 
-	alert('You scoreed '+score+'out of'+total);
+	alert('You scored '+score+' out of '+total);
 	return false;
 	 
-	 
+	
 }
